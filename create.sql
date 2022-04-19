4034e683d026da9c652a2029d0365c1eab9ecc3f
Shruti <sharma224@wisc.edu>
2022-04-19 04:28:26 -0500

    updated parser and sql commands in create.sql file

---
+++ create.sql
@@ -0,0 +1,43 @@
+drop table if exists Item;
+create table Item(
+    ItemID int NOT NULL,
+    Name varchar(255) NOT NULL,
+    Currently double NOT NULL,
+    First_Bid  double NOT NULL,
+    SellerID varchar(255) NOT NULL,
+    Number_Of_Bids int NOT NULL,
+    Buy_Price double, 
+    Started double NOT NULL, 
+    Ends double NOT NULL, 
+    Description varchar(255) NOT NULL, 
+    PRIMARY KEY (ItemID),
+    FOREIGN KEY (SellerID) REFERENCES Seller(UserID)
+);
+
+drop table if exists Bid;
+create table Bid (
+    ItemID int NOT NULL, 
+    Amount double NOT NULL, 
+    Time TIMESTAMP NOT NULL,
+    UserID varchar(255) NOT NULL, 
+    Rating varchar(255) NOT NULL, 
+    Location varchar(255), 
+    Country varchar(255),
+    Primary Key (ItemID, UserID),
+    FOREIGN KEY (ItemID) REFERENCES Items(ItemID)
+);
+
+drop table if exists Seller;
+create table Seller (
+    UserID int NOT NULL, 
+    Rating varchar(255) NOT NULL, 
+    PRIMARY KEY (UserID)
+);
+
+drop table if exists Category;
+create table Category (
+    ItemID int NOT NULL, 
+    CategoryName varchar(255) NOT NULL, 
+    PRIMARY KEY (ItemID),
+    FOREIGN KEY (ItemID) REFERENCES Items (ItemID)
+);
---
+++ parser.py
@@ -0,0 +1,163 @@
+
+"""
+FILE: skeleton_parser.py
+------------------
+Author: Firas Abuzaid (fabuzaid@stanford.edu)
+Author: Perth Charernwattanagul (puch@stanford.edu)
+Modified: 04/21/2014
+
+Skeleton parser for CS564 programming project 1. Has useful imports and
+functions for parsing, including:
+
+1) Directory handling -- the parser takes a list of eBay json files
+and opens each file inside of a loop. You just need to fill in the rest.
+2) Dollar value conversions -- the json files store dollar value amounts in
+a string like $3,453.23 -- we provide a function to convert it to a string
+like XXXXX.xx.
+3) Date/time conversions -- the json files store dates/ times in the form
+Mon-DD-YY HH:MM:SS -- we wrote a function (transformDttm) that converts to the
+for YYYY-MM-DD HH:MM:SS, which will sort chronologically in SQL.
+
+Your job is to implement the parseJson function, which is invoked on each file by
+the main function. We create the initial Python dictionary object of items for
+you; the rest is up to you!
+Happy parsing!
+"""
+
+import sys
+from json import loads
+from re import sub
+import os
+
+columnSeparator = "|"
+
+# Dictionary of months used for date transformation
+MONTHS = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06',\
+        'Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
+
+"""
+Returns true if a file ends in .json
+"""
+def isJson(f):
+    return len(f) > 5 and f[-5:] == '.json'
+
+"""
+Converts month to a number, e.g. 'Dec' to '12'
+"""
+def transformMonth(mon):
+    if mon in MONTHS:
+        return MONTHS[mon]
+    else:
+        return mon
+
+"""
+Transforms a timestamp from Mon-DD-YY HH:MM:SS to YYYY-MM-DD HH:MM:SS
+"""
+def transformDttm(dttm):
+    dttm = dttm.strip().split(' ')
+    dt = dttm[0].split('-')
+    date = '20' + dt[2] + '-'
+    date += transformMonth(dt[0]) + '-' + dt[1]
+    return date + ' ' + dttm[1]
+
+"""
+Transform a dollar value amount from a string like $3,453.23 to XXXXX.xx
+"""
+
+def transformDollar(money):
+    if money == None or len(money) == 0:
+        return money
+    return sub(r'[^\d.]', '', money)
+
+"""
+Parses a single json file. Currently, there's a loop that iterates over each
+item in the data set. Your job is to extend this functionality to create all
+of the necessary SQL tables for your database.
+"""
+def parseJson(json_file):
+    with open(json_file, 'r') as f:
+        items = loads(f.read())['Items'] # creates a Python dictionary of Items for the supplied json file
+
+        items_dat = open("Items.dat", "a")
+        category_dat = open("Category.dat", "a")
+        bid_dat = open("Bid.dat", "a")
+        seller_dat = open("Seller.dat", "a")
+
+        for item in items:
+            """
+            TODO: traverse the items dictionary to extract information from the
+            given `json_file' and generate the necessary .dat files to generate
+            the SQL tables based on your relation design
+            """
+            # TODO: double check that all attributes are used (BUY PRICE)
+            # NOTE: escape double quotes and null strings are NONE
+
+            #QUESTION: We haven't used the transform dollar and dttm functions provided
+            
+            # Fill Items.dat
+            name = "\"" + item["Name"].replace("\"", "\"\"") + "\"" if item["Name"] else "\"NONE\""
+#             currently = "\"" + item["Currently"].replace("\"", "\"\"") + "\"" if item["Currently"] else "\"NONE\""
+            currently = transformDollar(item['Currently']) 
+#             first_bid = "\"" + item["First_Bid"].replace("\"", "\"\"") + "\"" if item["First_Bid"] else "\"NONE\""
+            first_bid = transformDollar(item['First_Bid']) 
+            seller_id = "\"" + item["Seller"]["UserID"].replace("\"", "\"\"") + "\"" if item["Seller"]["UserID"] else "\"NONE\""
+            # buy_price = "\"" + item["Buy_Price"].replace("\"", "\"\"") + "\"" if item["Buy_Price"] else "\"NONE\""
+#             started = "\"" + item["Started"].replace("\"", "\"\"") + "\"" if item["Started"] else "\"NONE\""
+            started = transformDttm(item['Started'])
+#             ends = "\"" + item["Ends"].replace("\"", "\"\"") + "\"" if item["Ends"] else "\"NONE\""
+            ends = transformDttm(item['Ends'])
+            description = "\"" + item["Description"].replace("\"", "\"\"") + "\"" if item["Description"] else "\"NONE\""
+            # create one row of Items dat file
+            items_row = [item["ItemID"], name, currently, first_bid, seller_id, item["Number_of_Bids"], started, ends, description]
+            items_row = map(str, items_row)
+            items_row = "|".join(items_row) + "\n"
+            # append to Items dat file
+            items_dat.write(items_row)
+
+            # Fill Category.dat
+            if item["Category"]:
+                for org_cat in item["Category"]:
+                    cat = "\"" + org_cat.replace("\"", "\"\"") + "\""
+                    cat_row = item["ItemID"] + "|" + cat + "\n"
+                    category_dat.write(cat_row)
+
+            # Fill Sellers.dat
+            sell_row = seller_id + "|" + item["Seller"]["Rating"] + "\n"
+            seller_dat.write(sell_row)
+
+            # Fill Bid.dat
+            if item["Number_of_Bids"] != "0":
+                for bid in item["Bids"]:
+                    pass
+  
+        items_dat.close()
+        category_dat.close()
+        bid_dat.close()
+        seller_dat.close()
+
+"""
+Loops through each json files provided on the command line and passes each file
+to the parser
+"""
+def main(argv):
+    if len(argv) < 2:
+        print(sys.stderr, 'Usage: python skeleton_json_parser.py <path to json files>')
+        sys.exit(1)
+
+    if(os.path.exists("Items.dat")):
+        os.remove("Items.dat")
+    if(os.path.exists("Category.dat")):
+        os.remove("Category.dat")
+    if(os.path.exists("Bid.dat")):
+        os.remove("Bid.dat")
+    if(os.path.exists("Seller.dat")):
+        os.remove("Seller.dat")
+    
+    # loops over all .json files in the argument
+    for f in argv[1:]:
+        if isJson(f):
+            parseJson(f)
+            print ("Success parsing " + f)
+
+if __name__ == '__main__':
+    main(sys.argv)
