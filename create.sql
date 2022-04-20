c4de550bf57bcc008c003024c2e643390a9f8013
cass9918 <71353448+cass9918@users.noreply.github.com>
2022-04-20 13:32:17 -0500

    everything works upto load.txt

--- create.sql
+++ create.sql
@@ -1,26 +1,26 @@
-drop table if exists Item;
-create table Item(
-    ItemID int NOT NULL,
-    Name varchar(255) NOT NULL,
-    Currently double NOT NULL,
-    First_Bid  double NOT NULL,
-    SellerID varchar(255) NOT NULL,
-    Number_Of_Bids int NOT NULL,
+drop table if exists Items;
+create table Items(
+    ItemID int,
+    Name varchar(255),
+    Currently double,
+    First_Bid  double,
+    SellerID varchar(255),
+    Number_Of_Bids int,
     Buy_Price double, 
-    Started double NOT NULL, 
-    Ends double NOT NULL, 
-    Description varchar(255) NOT NULL, 
+    Started double, 
+    Ends double, 
+    Description varchar(255), 
     PRIMARY KEY (ItemID),
     FOREIGN KEY (SellerID) REFERENCES Seller(UserID)
 );
 
 drop table if exists Bid;
 create table Bid (
-    ItemID int NOT NULL, 
-    Amount double NOT NULL, 
-    Bid_Time TIMESTAMP NOT NULL,
-    UserID varchar(255) NOT NULL, 
-    Rating int NOT NULL, 
+    ItemID int, 
+    Amount double, 
+    Bid_Time TIMESTAMP,
+    UserID varchar(255), 
+    Rating int, 
     Location varchar(255), 
     Country varchar(255),
     Primary Key (ItemID, UserID),
@@ -29,15 +29,15 @@ create table Bid (
 
 drop table if exists Seller;
 create table Seller (
-    UserID int NOT NULL, 
-    Rating int NOT NULL, 
+    UserID int, 
+    Rating int, 
     PRIMARY KEY (UserID)
 );
 
 drop table if exists Category;
 create table Category (
-    ItemID int NOT NULL, 
-    CategoryName varchar(255) NOT NULL, 
+    ItemID int, 
+    CategoryName varchar(255), 
     PRIMARY KEY (ItemID),
     FOREIGN KEY (ItemID) REFERENCES Items (ItemID)
 );
--- runParser.sh
+++ runParser.sh
@@ -1,3 +1,13 @@
-rm *.dat
+rm Items.dat
+rm Category.dat
+rm Bid.dat
+rm Seller.dat
 python parser.py ebay_data/items-*.json
-sort -u *.dat
+sort Items.dat > Items.dat
+sort Category.dat > Category.dat
+sort Bid.dat > Bid.dat
+sort Seller.dat > Seller.dat
+uniq Items.dat > Items.dat
+uniq Category.dat Category.dat
+uniq Bid.dat > Bid.dat
+uniq Seller.dat > Seller.dat
